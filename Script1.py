53de9ffdcbd635a6a12f69e4f5302e15b548b725
Clementwdk <wodkowskiclement@orange.fr>
2020-11-25 14:20:15 +0000

    first commit

---
+++ Script1.py
@@ -0,0 +1,16 @@
+#Write a program to ask the user for their baggage weight
+#and notify them if it exceeds the 15kg allowance
+totalAllowance=60
+totalBaggage=0
+while totalBaggage<totalAllowance:
+    weight=int(input('Enter baggage weight: '))
+    if weight<10:
+        print('Bag may be brought on board')
+    elif weight<=15:
+        print('Bag may be carried')
+    elif weight <=30:
+        print('Baggage allowance of 15kg exceeded, extra charges will apply')
+    else:
+        print('Bag is too heavy!')
+    totalBaggage=totalBaggage+weight
+print('Thank you')
